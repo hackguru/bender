ffc11d0c7b66097679c1b7d251bbd2a625a6b642
Edward Mehr <hackguru@Edwards-MacBook-Air.local>
2016-05-16 15:28:59 -0700

    logical solver is more efficient now

--- logical-auto-solver.py
+++ logical-auto-solver.py
@@ -2,6 +2,16 @@
 
 # solving a**2 = k*n+1
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 import sys
 import math
 import sympy
@@ -10,6 +20,10 @@ import shutil
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.core.mul import Mul
 from sympy.core.add import Add
+from sympy.logic.boolalg import to_cnf
+
+shouldSubN = True
+shouldSolve = True
 
 N = int(input("Number to factorize: "))
 
@@ -53,7 +67,7 @@ def InitialWallaceWeights(iName, i, jName, j, k, toAdd, leftOvers):
 		for l in range(0,count,3):
 			if l+2<count:
 				leftOvers.append(((interim[l]^interim[l+1])&interim[l+2])|(interim[l]&interim[l+1]))
-				nextInterim.append(interim[l]^interim[l+1])
+				nextInterim.append(interim[l]^interim[l+1]^interim[l+2])
 			elif l+1<count:
 				leftOvers.append(interim[l]&interim[l+1])
 				nextInterim.append(interim[l]^interim[l+1])
@@ -61,90 +75,57 @@ def InitialWallaceWeights(iName, i, jName, j, k, toAdd, leftOvers):
 				nextInterim.append(interim[l])
 		interim = nextInterim
 		count = len(interim)
-	#print("Run {}:".format(k))
-	#print(toAdd)
-	#print(toReturn)
-	#print(leftOvers)
 	return toReturn
 
-equalities = [
-	{
-		D['a0']:True, # we assume a is odd - even can be calculated once we have odd
-		D['n0']:True, # if we substitute n or not it always will be odd so we might as well
-	#	D['k0']:False,
-	},
-	#{
-	#	D['k1']:False,
-	#},
-	#{
-    	#	D['k2']:False,
-	#},
-	#{
-	#	D['k3']:parse_expr('a1|a2'),
-	#},
-	#{
-	#	D['k4']:parse_expr('((a1|~a3)&(a2|a3)&(a3|~a1))^(n1&(a1|a2))'),
-	#},
-	#{
-	#	D['k5']:parse_expr('(a2 & n2 & ~a1) | (a2 & n2 & ~a3) | (a4 & ~a1 & ~a2) | (a1 & a2 & a3 & ~n2) | (a1 & a3 & ~a4 & ~n2) | (a1 & n2 & ~a3 & ~a4) | (a3 & a4 & n2 & ~a2)| (a4 & ~a2 & ~a3 & ~n2)'),
-	#},
-	#{
-	#},
-	#{
-	#},
-]
-#p = sympy.Wild('p')
-#q = sympy.Wild('q')
-updates = {
-	#p+q: 2*p*q + ((p+q)%2),
+leftSideKnowns = {
+	D['a0']:True, # we assume a is odd - even can be calculated once we have odd
+}
+rightSideKnowns = {
+	D['n0']:True, # if we substitute n or not it always will be odd so we might as well
+}
+substitutes = {
 }
 equations = []
+toAddToLeftSide=[]
+toAddToRightSide=[]
+satEquation = True 
+i = 0
+while i < 2*n:
+	leftSideLeftOvers=[]
+	leftSide = sympy.simplify_logic(functools.reduce(lambda x,y:x^y, [False]+InitialWallaceWeights('a',n,'a',n,i,toAddToLeftSide,leftSideLeftOvers)).subs(leftSideKnowns))
+	# EXCEPTION - MOVING -1 TO LEFT SIDE OF EQUATION TO SUBTRACT:
+	if i==0:
+		leftSide ^= 1
+	leftSideLeftOvers = [sympy.simplify_logic(leftOverTerm.subs(leftSideKnowns)) for leftOverTerm in leftSideLeftOvers]
+	
+	rightSideLeftOvers=[]
+	rightSide = functools.reduce(lambda x,y:x^y, [False]+InitialWallaceWeights('n',n,'k',n,i,toAddToRightSide,rightSideLeftOvers)).subs(rightSideKnowns).subs(substitutes)
+	rightSideLeftOvers = [rightSideTerm.subs(rightSideKnowns).subs(substitutes) for rightSideTerm in rightSideLeftOvers]
+	# replacing n digits
+	if shouldSubN:
+	    rightSide = rightSide.subs(n_Subs)
+	    rightSideLeftOvers = [rightSideTerm.subs(n_Subs) for rightSideTerm in rightSideLeftOvers]
+	
+	toAddToLeftSide = leftSideLeftOvers
+	toAddToRightSide = rightSideLeftOvers
+	
+	equations.append((leftSide,rightSide))
+	
+	# adding new term to equalitis for new k
+	newKkey = 'k{}'.format(len(equations)-1)
+	if newKkey in D:
+		substitutes[D[newKkey]] = equations[-1][0]^sympy.simplify_logic(equations[-1][1]^D[newKkey])
+	else:
+		satEquation = sympy.Equivalent(equations[-1][0],sympy.simplify_logic(equations[-1][1])) & satEquation
+	print((bcolors.HEADER+"w:{:>2}"+bcolors.ENDC+" {:>"+str(math.floor(shutil.get_terminal_size((80,20))[0]/2)-2-3)+"}").format(i,sympy.pretty(equations[-1][0]))+bcolors.OKGREEN+" = "+bcolors.ENDC+sympy.pretty(equations[-1][1]))
+	i += 1 
 
-def reweighAndPrint():
-	toPrint = []
-	toAddToLeftSide=[]
-	toAddToRightSide=[]
-	satEquation = True
-	i = 0
-	while i < 2*n:
-		leftSideLeftOvers=[]
-		leftSide = functools.reduce(lambda x,y:x^y, [False]+InitialWallaceWeights('a',n,'a',n,i,toAddToLeftSide,leftSideLeftOvers))
-		# EXCEPTION - MOVING -1 TO LEFT SIDE OF EQUATION TO SUBTRACT:
-		if i==0:
-			leftSide ^= 1
-		#print("left side:")
-		#print(leftSide)
-		rightSideLeftOvers=[]
-		rightSide = functools.reduce(lambda x,y:x^y, [False]+InitialWallaceWeights('n',n,'k',n,i,toAddToRightSide,rightSideLeftOvers))
-		#print("right side")
-		#print(rightSide)
-		# replacing n digits
-		#rightSide = rightSide.subs(n_Subs)
-		for j in equalities[:min(i+1,len(equalities))]:
-			leftSide = leftSide.subs(j)
-			rightSide = rightSide.subs(j)
-		toAddToLeftSide = leftSideLeftOvers
-		toAddToRightSide = rightSideLeftOvers
-		equations.append((leftSide,rightSide))
-		# adding new term to equalitis for new k
-		newKkey = 'k{}'.format(len(equations)-1)
-		if newKkey in D:
-		    equalities.append({D[newKkey]: sympy.simplify_logic(equations[-1][0]^equations[-1][1]^D[newKkey])})
-		else:
-		    satEquation = sympy.Equivalent(equations[-1][0],equations[-1][1]) & satEquation
-		i += 1
-	for i,j in enumerate(equations):
-		if str(sympy.simplify_logic(j[0])) != str(sympy.simplify_logic(j[1])):
-			toPrint.append(("w:{:>2} {:>"+str(math.floor(shutil.get_terminal_size((80,20))[0]/2)-2-3)+"}").format(i,sympy.pretty(sympy.simplify_logic(j[0])))+" = "+sympy.pretty(j[1]))
-	print("\n".join(toPrint))
-	#print(sympy.simplify_logic(sympy.Equivalent(equations[-1][0],equations[-1][1])))
-	#print("Stisafiable models:")
-	#print("\n".join(map(str,list(sympy.satisfiable(sympy.Equivalent(equations[-1][0],equations[-1][1]),all_models=True)))))
-	#print(sympy.pretty(equations[-1][1]^D['k5']))
-	#print("====== New term for k{} ======".format(len(equations)-1))
-	#print(sympy.pretty(sympy.simplify_logic(equations[-1][0]^equations[-1][1]^D['k{}'.format(len(equations)-1)])))
-	print("====Equalities dict=====")
-	print(equalities)
-	print("======Sat Equation======")
-	print(sympy.simplify_logic(satEquation))
-reweighAndPrint()
+#print("number of terms in CNF for satisfiability: {}".format(len(to_cnf(satEquation.args))))
+
+if shouldSubN & shouldSolve:
+    print("=======Sat Equation=======")
+    simplifiedSat = sympy.simplify_logic(satEquation)
+    print(simplifiedSat)
+    print("=Satisfiability solutions=")
+    allModels = list(sympy.satisfiable(simplifiedSat,all_models=True))
+    print(", ".join(map(str,[2*functools.reduce(lambda x,y:2*x+y, [0]+[int(model[D[x]]) for x in ["a{}".format(i+1) for i in reversed(range(n-1))]])+1 for model in allModels])))
